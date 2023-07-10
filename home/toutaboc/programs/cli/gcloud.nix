b4eca1d189e4112a5de30501b33f671021bc4f45
Gustavo Belfort <gustavo@belfort.dev>
2023-07-10 14:42:28 -0300

    PATCH: Add cloud sql proxy command to gcloud

--- home/toutaboc/programs/cli/gcloud.nix
+++ home/toutaboc/programs/cli/gcloud.nix
@@ -5,6 +5,7 @@
       google-cloud-sdk.withExtraComponents [
         google-cloud-sdk.components.cloud-build-local
         google-cloud-sdk.components.gke-gcloud-auth-plugin
+        google-cloud-sdk.components.cloud_sql_proxy
       ]
     )
   ];
