cd7223cd8822b5c796065104530443b07c82db24
gustavobelfort <gustavo@belfort.dev>
2023-09-04 13:19:14 -0300

    FEAT: Install vscode

---
+++ modules/editors/vscode/extensions.nix
@@ -0,0 +1,41 @@
+{ pkgs, ... }:
+
+with pkgs.vscode-utils;
+let
+  extensions = with pkgs.vscode-extensions; [
+    tomoki1207.pdf
+    usernamehw.errorlens
+    fabiospampinato.vscode-diff
+    retepaskab.relativegoto
+
+    # theme
+    file-icons.file-icons
+    gruntfuggly.todo-tree
+    github.github-vscode-theme
+
+    # markdown
+    yzhang.markdown-all-in-one
+
+    #languages
+    bbenoist.nix
+    hashicorp.terraform
+    ms-kubernetes-tools.vscode-kubernetes-tools
+
+    # rest
+    humao.rest-client
+  ];
+
+  fabiospampinato.vscode-diff = extensionFromVscodeMarketplace {
+    name = "vscode-diff";
+    publisher = "fabiospampinato";
+    version = "1.4.2";
+    sha256 = "sha256-b1N+m+Y4kUylXrJOU0Y1c9eRI12CSkb5mWyKYy+FAzc=";
+  };
+
+  retepaskab.relativegoto = extensionFromVscodeMarketplace {
+    name = "relativegoto";
+    publisher = "retepaskab";
+    version = "0.0.1";
+    sha256 = "sha256-8xg8cy2OtJ0CdEclE6grIAHXHHN5eBfNKYAqu4sk6y8=";
+  };
+in extensions
---
+++ modules/editors/vscode/settings.nix
@@ -0,0 +1,89 @@
+{
+  "breadcrumbs.enabled" = true;
+  "breadcrumbs.filePath" = "last";
+
+  "editor.cursorBlinking" = "solid";
+  "editor.cursorSmoothCaretAnimation" = "off";
+  "editor.cursorStyle" = "underline";
+  "editor.cursorSurroundingLines" = 5;
+  "editor.lineNumbers" = "relative";
+
+  "editor.fontFamily" = "'Jetbrains Mono'";
+  "editor.fontLigatures" = true;
+  "editor.fontSize" = 12.01;
+
+  "editor.minimap.enabled" = false;
+  "editor.renderWhitespace" = "none";
+  "editor.formatOnSave" = true;
+  "editor.rulers" = [ 120 ];
+  "editor.smoothScrolling" = true;
+  "editor.scrollBeyondLastLine" = false;
+  "editor.stickyScroll.enabled" = true;
+  "editor.multiCursorModifier" = "ctrlCmd";
+  "editor.find.autoFindInSelection" = "never";
+  "editor.suggest.preview" = true;
+
+  "explorer.autoReveal" = true;
+  "explorer.openEditors.visible" = 1;
+  "explorer.confirmDelete" = false;
+  "explorer.confirmDragAndDrop" = false;
+
+  "extensions.ignoreRecommendations" = true;
+
+  "files.autoSave" = "onFocusChange";
+  "files.exclude"."**/.git" = true;
+  "files.insertFinalNewline" = true;
+  "files.trimTrailingWhitespace" = true;
+  "files.maxMemoryForLargeFilesMB" = 1024;
+  "files.watcherExclude" = {
+    "**/.git/objects/**" = true;
+    "**/.git/subtree-cache/**" = true;
+    "**/node_modules/**" = true;
+  };
+
+  "telemetry.telemetryLevel" = "off";
+
+  "terminal.integrated.cursorBlinking" = false;
+  "terminal.integrated.cursorStyle" = "underline";
+  "terminal.integrated.fontFamily" = "JetBrains Mono";
+  "terminal.integrated.fontSize" = 12.01;
+  "terminal.explorerKind" = "external";
+
+  "update.mode" = "none";
+
+  "window.title" = "\${dirty}  \${rootName}\${separator}\${activeEditorMedium}";
+  "window.titleBarStyle" = "custom";
+  "window.commandCenter" = true;
+  "window.autoDetectColorScheme" = true;
+  "window.newWindowDimensions" = "offset";
+
+  "workbench.colorTheme" = "GitHub Dark";
+  "workbench.preferredLightColorTheme" = "GitHub Light";
+  "workbench.preferredDarkColorTheme" = "GitHub Dark";
+  "workbench.editor.enablePreviewFromQuickOpen" = false;
+  "workbench.iconTheme" = "file-icons";
+  "workbench.settings.enableNaturalLanguageSearch" = false;
+  "workbench.startupEditor" = "none";
+  "workbench.activityBar.visible" = true;
+  "workbench.statusBar.visible" = true;
+  "workbench.tips.enabled" = false;
+  "workbench.tree.indent" = 18;
+  "workbench.tree.renderIndentGuides" = "always";
+  "workbench.panel.defaultLocation" = "bottom";
+  "workbench.layoutControl.enabled" = false;
+
+  "yaml.format.bracketSpacing" = false;
+
+  "zenMode.centerLayout" = false;
+  "zenMode.fullScreen" = false;
+  "zenMode.hideLineNumbers" = false;
+  "zenMode.hideStatusBar" = false;
+
+  "[markdown]" = {
+    "editor.wordWrap" = "bounded";
+    "editor.wordWrapColumn" = 1000;
+  };
+
+  # usernamehw.errorlens extension
+  "errorLens.gutterIconsEnabled" = true;
+}
