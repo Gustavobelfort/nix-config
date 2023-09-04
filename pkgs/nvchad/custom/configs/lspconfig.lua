8d3da85f1d57e8bad6f86f5910c9d8ed6b2e6a4f
gustavobelfort <gustavo@belfort.dev>
2023-09-04 15:25:19 -0300

    FEAT: Install nvchad

--- pkgs/nvchad/custom/configs/lspconfig.lua
+++ pkgs/nvchad/custom/configs/lspconfig.lua
@@ -42,7 +42,7 @@ lspconfig["yamlls"].setup{
   settings = {
     yaml = {
       format = {
-        bracketSpacing = false,
+        bracketSpacing = "false",
       },
       schemas = {
         ["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0-standalone-strict/all.json"] = "/*.k8s.yaml",
